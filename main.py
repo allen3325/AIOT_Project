fa87b788bbe11dd1cc715eb1e0488a34980a0942
allen3325 <00857146@email.ntou.edu.tw>
2023-12-17 12:12:06 +0000

    push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 from crawler.craw_chrome import Crawler
 
 app = FastAPI()
@@ -10,9 +10,15 @@ async def root():
     return {"message": "Hello"}
 
 
-@app.get("/analyze")
-async def emotion_analyze(url: str):
+class Url_Body(BaseModel):
+    url: str
+
+@app.post("/analyze")
+async def emotion_analyze(url: Url_Body):
     crawler = Crawler()
-    comment_list = crawler.fetch_comment(
-        url="https://www.google.com/maps/place/%E4%B8%AD%E8%88%88%E5%A4%A7%E5%AD%B8%E7%B6%9C%E5%90%88%E6%95%99%E5%AD%B8%E5%A4%A7%E6%A8%93/@24.1222692,120.6721234,17z/data=!3m1!5s0x34693cfdf0f370f7:0xb5f97dcd8f1e9311!4m18!1m9!3m8!1s0x34693cfcecffe9d9:0xe28afadc0dad203a!2z5ZyL56uL5Lit6IiI5aSn5a24!8m2!3d24.123552!4d120.675326!9m1!1b1!16zL20vMDR4cDF6!3m7!1s0x34693cfdf13d560b:0xa4002a1f1219e2dd!8m2!3d24.1218109!4d120.6726999!9m1!1b1!16s%2Fg%2F11gvxx_g3b?entry=ttu")
+    # comment_list = crawler.fetch_comment(
+    #     url="https://www.google.com/maps/place/%E7%9B%92%E5%AD%90%E5%A3%BD%E5%8F%B8(%E8%88%88%E5%A4%A7%E5%BA%97)/@24.1180778,120.6715122,19.17z/data=!4m18!1m9!3m8!1s0x34693cfcecffe9d9:0xe28afadc0dad203a!2z5ZyL56uL5Lit6IiI5aSn5a24!8m2!3d24.123552!4d120.675326!9m1!1b1!16zL20vMDR4cDF6!3m7!1s0x34693cfc1ff85e37:0x78b6cde9f47e6d9!8m2!3d24.1185374!4d120.6717073!9m1!1b1!16s%2Fg%2F11c31q0wns?entry=ttu"
+    # )
+    print(url.url)
+    comment_list = crawler.fetch_comment(url=url.url)
     return comment_list
